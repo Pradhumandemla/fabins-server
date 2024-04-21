238e4edcaf62e7b5e9afffd8ef3ae516f392fb99
Pradhuman Demla <pradhumnd33@gmail.com>
2024-04-22 04:40:27 +0530

    initials
    Signed-off-by: Pradhuman Demla <pradhumnd33@gmail.com>

---
+++ models/Post.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+// const postSchema = mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.ObjectId,
+//       ref: 'User',
+//       required: true,
+//     },
+//     firstName: {
+//       type: String,
+//       required: true,
+//     },
+//     lastName: {
+//       type: String,
+//       required: true,
+//     },
+//     location: String,
+//     description: String,
+//     picturePath: String,
+//     userPicturePath: String,
+//     likes: {
+//       type: Map,
+//       of: Boolean,
+//     },
+//     comments: {
+//       type: Array,
+//       default: [],
+//     },
+//   },
+//   { timestamps: true }
+// );
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required:true,
+    },
+    description: String,
+    images: [{
+      path:String,
+      size:Number
+    }], 
+    likes: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        }
+    ],
+    comments: [{
+      userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+      },
+      comment: String
+    }],
+}, { timestamps: true })
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
