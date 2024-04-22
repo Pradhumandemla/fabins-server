50f6509eea41fe6c5fa8e033daf5e21c17f52fab
Pradhuman Demla <pradhumnd33@gmail.com>
2024-04-22 05:46:20 +0530

    initials
    Signed-off-by: Pradhuman Demla <pradhumnd33@gmail.com>

--- index.js
+++ index.js
@@ -66,18 +66,19 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3001;
 mongoose.set('strictQuery', true);
-// mongoose
-//   .connect(process.env.MONGO_URL, {
-//     useNewUrlParser:true, 
-//     useUnifiedTopology:true
-//   })
-//   .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser:true, 
+    useUnifiedTopology:true
+  })
+  .then(() => {
+    console.log('db connection established');
 
     /* ADD DATA ONE TIME */
     // User.insertMany(users);
     // Post.insertMany(posts);
-  // })
-  // .catch((error) => {
-  //   console.log(`${error} did not connect`);
-  // });
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });
